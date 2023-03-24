aec6cdf3c6759ca6d5ee2fef07fb172d4d165cdc
salcasta <salcasta@gmail.com>
2023-03-23 19:40:15 -0500

    basics of ruby, variables, math, and methods

---
+++ introduction_to_ruby.rb
@@ -0,0 +1,149 @@
+# Data Types: Numbers, Strings, Booleans
+
+# 1. Set the following variables to the corresponding values:
+
+# my_num to the value 25
+# my_boolean to the value true (note the capitalization!)
+# my_string to the value "Ruby"
+
+# my_num =  25  
+# my_boolean = true    
+# my_string = "Ruby" 
+
+
+
+# Variables
+
+# 1. Set the variable my_num to the value 100, then click the Run button to run your code.
+
+# my_num = 100
+
+
+# Math
+
+# 1. Do a little math practice in the editor. When you’re ready, click Next.
+
+# 2**3
+
+
+# 'puts' and 'print'
+
+# 1. In the editor, use at least one print statement and at least one puts statement. You can print out any strings you like! (Make sure to put your strings between quotes, like this: "Hello!".)
+
+# print "Don't forget"
+
+# puts  " Your initial savepoint"
+
+
+# Everything in Ruby is an Object
+
+# The '.length' Method
+
+# 1. Call the .length method on your name (remember to use quotes around your name).
+
+#     puts the answer to the console if you want to see the value.
+
+# puts "Salvador".length
+
+
+# The '.reverse' Method
+
+# 1. Call the .reverse method on your name. Don’t forget those quotation marks around your name to make   it a string!
+
+#     puts the answer to the console if you want to see the value.
+
+
+# puts "Salvador".reverse
+
+
+
+# '.upcase' & '.downcase'
+
+# 1. Call .upcase on your name to make your name ALL CAPS and use puts to print it to the screen, like this:
+
+# puts "eric".upcase    # ==> ERIC
+# On the next line, call .downcase to make your name all lower case. Make sure to use puts so you can see it printed out!
+
+
+# puts "salvador".upcase
+# puts "SALVADOR".downcase
+
+
+
+# Single-Line Comments
+
+# 1. Write a comment in the editor. It can be anything you like!
+
+# ### hello this is a comment 
+# puts "salvador".length
+
+
+
+# Multi-Line Comments
+
+# 1. Create a multi-line comment in the editor to the right. Make sure =begin and =end are on their own lines!
+
+
+# ###
+# =begin
+# hello
+# this 
+# is
+# just
+# comments
+# =end
+
+
+# Naming Conventions
+
+# 1. Create a variable name in the editor and set it equal to your name as a string (between quotes, like this: "Eric"). Your string can be capitalized, but name should be all lower case!
+
+# name_variable = "Sal"
+
+
+
+# Variables & Data Types
+
+# 1. Create a variable called my_name and set it equal to your name as a string. Create a second variable called my_age and set it equal to your age as a number (don’t use any quotes around the number).
+
+# my_name = "Sal"
+# my_age = 35
+
+
+# print my_name
+
+
+# Math
+
+# 1. In the editor, set the variables:
+
+#     sum equal to 13 + 379
+#     product equal to 923 * 15
+#     quotient equal to 13209 / 17
+#     Print each variable to the console using puts if you want to see the results.
+
+# puts sum = 13 + 379
+# puts product = 923 * 15
+# puts quotient = 13209 / 17
+
+
+# Strings and String Methods
+
+# 1. Declare a variable name and set it equal to a string containing your name. Call .downcase on your name to make it all lower case, call .reverse on your lowercase name to make it backwards, then call .upcase on your backwards name to make it ALL CAPS.
+
+# In Ruby, you can do this two ways: each method call on a separate line, or you can chain them together, like this:
+
+# name.method1.method2.method3
+
+# puts the method call if you want to see the result.
+
+# puts name = "Salvador".downcase
+# puts name = "Salvador".reverse
+# puts name = "Salvador".upcase
+
+# # or can be done like this
+
+# puts name = "Salvador".downcase.reverse
+# puts name = "Salvador".upcase.reverse
+
+# puts name = "Salvador".downcase.reverse.length
